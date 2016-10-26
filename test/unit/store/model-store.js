13e362069a4b3ef71bfa8b3ca9aa472b31152750
Trevin Hofmann <trevinhofmann@gmail.com>
2016-10-26 09:44:37 -0500

    Test the model store

--- test/unit/store/model-store.js
+++ test/unit/store/model-store.js
@@ -1,15 +1,41 @@
-import { Dispatcher } from 'consus-core/flux';
 import ModelStore from '../../../.dist/store/model-store';
 import { assert } from 'chai';
+import { addAction } from '../../util/database';
 
 describe('ModelStore', () => {
 
+    let model;
+
     before(() => {
-        Dispatcher.handleAction('CLEAR_ALL_DATA');
+        addAction('CLEAR_ALL_DATA');
     });
 
     it('should instantiate without any models', () => {
         assert.lengthOf(ModelStore.getModels(), 0);
+    });
+
+    it('should create a model an retrieve it by the action id', () => {
+        addAction('NEW_MODEL', {
+            name: 'Transistor'
+        }).then(actionId => {
+            model = ModelStore.getModelByActionId(actionId);
+        });
+        assert.lengthOf(ModelStore.getModels(), 1);
+    });
+
+    it('should retrieve a model by address', () => {
+        assert.strictEqual(ModelStore.getModelByAddress(model.address), model);
+    });
+
+    it('should create more models', () => {
+        addAction('NEW_MODEL', {
+            name: 'Resistor'
+        });
+        assert.lengthOf(ModelStore.getModels(), 2);
+        addAction('NEW_MODEL', {
+            name: 'Wire'
+        });
+        assert.lengthOf(ModelStore.getModels(), 3);
     });
 
 });
