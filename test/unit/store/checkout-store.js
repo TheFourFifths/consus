1af2fd36286bfcc6d66f72249a5864789d8f4cf6
Trevin Hofmann <trevinhofmann@gmail.com>
2016-10-26 19:30:35 -0500

    Test the checkout store

--- test/unit/store/checkout-store.js
+++ test/unit/store/checkout-store.js
@@ -1,19 +1,52 @@
-import { Dispatcher } from 'consus-core/flux';
 import CheckoutStore from '../../../.dist/store/checkout-store';
+import ModelStore from '../../../.dist/store/model-store';
+import ItemStore from '../../../.dist/store/item-store';
+import StudentStore from '../../../.dist/store/student-store';
 import { assert } from 'chai';
+import { addAction } from '../../util/database';
 
 describe('CheckoutStore', () => {
 
+    let items = [];
+    let student;
+
     before(() => {
-        Dispatcher.handleAction('CLEAR_ALL_DATA');
+        addAction('CLEAR_ALL_DATA');
+        addAction('NEW_MODEL', {
+            name: 'Resistor'
+        }).then(actionId => {
+            let model = ModelStore.getModelByActionId(actionId);
+            for (let i = 0; i < 3; i++) {
+                addAction('NEW_ITEM', {
+                    modelAddress: model.address
+                }).then(actionId => {
+                    items.push(ItemStore.getItemByActionId(actionId));
+                });
+            }
+        });
+        addAction('NEW_STUDENT', {
+            id: '123456',
+            name: 'John von Neumann'
+        }).then(actionId => {
+            student = StudentStore.getStudentByActionId(actionId);
+        });
     });
 
     it('should instantiate without any checkouts', () => {
         assert.lengthOf(CheckoutStore.getCheckouts(), 0);
     });
 
-    it('should instantiate with any checkout errors', () => {
+    it('should instantiate without any checkout errors', () => {
         assert.lengthOf(CheckoutStore.getCheckoutErrors(), 0);
+    });
+
+    it('should create a checkout', () => {
+        addAction('NEW_CHECKOUT', {
+            studentId: student.id,
+            items: [items[0].address, items[1].address]
+        }).then(() => {
+            assert.lengthOf(CheckoutStore.getCheckouts(), 1);
+        });
     });
 
 });
