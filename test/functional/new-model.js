dbee41826658e1d29e39b9f12f36af01e90f117c
Trevin Hofmann <trevinhofmann@gmail.com>
2017-01-08 19:48:09 -0600

    Test adding a model

--- test/functional/new-model.js
+++ test/functional/new-model.js
@@ -1,6 +1,8 @@
 import { assert } from 'chai';
 import { addAction } from '../util/database';
+import { post } from '../util/mock-client';
 import server from '../../.dist/lib/server';
+import ModelStore from '../../.dist/store/model-store';
 
 describe('New model', () => {
 
@@ -14,6 +16,37 @@ describe('New model', () => {
 
     beforeEach(() => {
         return addAction('CLEAR_ALL_DATA');
+    });
+
+    it('should add a model', () => {
+        assert.lengthOf(ModelStore.getModels(), 0);
+        return post('model', {
+            name: 'Widget',
+            description: 'A widget',
+            manufacturer: 'The Factory',
+            vendor: 'The Store',
+            location: 'The shelf',
+            isFaulty: false,
+            faultDescription: '',
+            price: 3.50,
+            count: 10
+        }).then(response => {
+            assert.lengthOf(ModelStore.getModels(), 1);
+            let model = ModelStore.getModelByAddress(response.address);
+            assert.deepEqual(response, model);
+            delete response.address;
+            assert.deepEqual(response, {
+                name: 'Widget',
+                description: 'A widget',
+                manufacturer: 'The Factory',
+                vendor: 'The Store',
+                location: 'The shelf',
+                isFaulty: false,
+                faultDescription: '',
+                price: 3.50,
+                count: 10
+            });
+        });
     });
 
 });
