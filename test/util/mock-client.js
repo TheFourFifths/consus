e7b4533b08748a0f88b69a9871e44eecd36c0829
Trevin Hofmann <trevinhofmann@gmail.com>
2017-01-08 19:30:39 -0600

    Add a mock client utility

---
+++ test/util/mock-client.js
@@ -0,0 +1,38 @@
+import request from 'request';
+
+function call(endpoint, method, qs, json) {
+    let options = {
+        uri: `http://localhost/api/${endpoint}`,
+        method
+    };
+    if (typeof qs !== 'undefined') {
+        options.qs = qs;
+    }
+    if (typeof json !== 'undefined') {
+        options.json = json;
+    }
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (typeof body === 'string') {
+                body = JSON.parse(body);
+            }
+            if (body.status === 'success') {
+                resolve(body.data);
+            } else {
+                reject(body.message);
+            }
+        });
+    });
+}
+
+export function del(endpoint, data) {
+    return call(endpoint, 'DELETE', data);
+}
+
+export function get(endpoint, data) {
+    return call(endpoint, 'GET', data);
+}
+
+export function post(endpoint, data) {
+    retrn call(endpoint, 'POST', undefined, data);
+}
