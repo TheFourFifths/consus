baf2556520ddd8f28e04ba34680612a01d35c6b5
Jordan Longato <longatoj@msoe.edu>
2017-04-05 17:47:55 -0500

    ha

--- test/unit/lib/email.js
+++ test/unit/lib/email.js
@@ -1,18 +1,18 @@
-// import { assert } from 'chai';
-// import StudentStore from '../../../.dist/store/student-store';
-// import * as email from '../../../.dist/lib/email';
-// import sinon from 'sinon';
-//
-// describe("Email system", () => {
-//     it("Sends an email to the correct students", () => {
-//         let getStudentsWithOverdueItems = sinon.stub(StudentStore, "getStudentsWithOverdueItems");
-//         getStudentsWithOverdueItems.returns([
-//             {email: 'trevinhofmann@gmail.com'}
-//         ]);
-//         let spy = sinon.spy(email, 'sendMail');
-//         return email.sendOverdueItemNotifications().then(() => {
-//             assert.isTrue(spy.called, "Spy should be called");
-//             StudentStore.getStudentsWithOverdueItems.restore();
-//         });
-//     }).timeout(15000);
-// });
+import { assert } from 'chai';
+import StudentStore from '../../../.dist/store/student-store';
+import * as email from '../../../.dist/lib/email';
+import sinon from 'sinon';
+
+describe("Email system", () => {
+    it("Sends an email to the correct students", () => {
+        let getStudentsWithOverdueItems = sinon.stub(StudentStore, "getStudentsWithOverdueItems");
+        getStudentsWithOverdueItems.returns([
+            {email: 'trevinhofmann@gmail.com'}
+        ]);
+        let spy = sinon.spy(email, 'sendMail');
+        return email.sendOverdueItemNotifications().then(() => {
+            assert.isTrue(spy.called, "Spy should be called");
+            StudentStore.getStudentsWithOverdueItems.restore();
+        });
+    }).timeout(15000);
+});
