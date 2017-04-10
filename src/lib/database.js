8232d0237713a2f10cff3636b221484c70021f30
Trevin Hofmann <trevinhofmann@gmail.com>
2017-04-09 21:30:27 -0500

    Persistent storage!

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,13 +1,37 @@
 import crypto from 'crypto';
+import path from 'path';
+import config from 'config';
 import { Dispatcher } from 'consus-core/flux';
+import Oak from 'oak-lite';
+
+let dataDirectory = config.get('database.data_directory');
+if (dataDirectory === '') {
+    dataDirectory = path.join(__dirname, '../../');
+}
+
+let database = Oak.configure({
+    dataDirectory
+});
+
+let rack = database.selectRack('actions');
+
+let promises = new Map();
+
+rack.subscribe(action => {
+    Dispatcher.handleAction(action.type, action.data);
+    promises.get(action.data.actionId)(action.data.actionId);
+    promises.delete(action.data.actionId);
+});
 
 export function addAction(type, data) {
-    // TODO: Write to persistent database
-    // TODO: Write to backup database
     data.actionId = crypto.randomBytes(32).toString('hex');
     data.timestamp = Math.floor(Date.now() / 1000);
+    let action = {
+        type,
+        data
+    };
+    rack.publish(action);
     return new Promise(resolve => {
-        Dispatcher.handleAction(type, data);
-        resolve(data.actionId);
+        promises.set(data.actionId, resolve);
     });
 }
