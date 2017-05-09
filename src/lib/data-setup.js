eb7c9a707b55a977bdf57a439cf4cbcb89bef0fa
Tony Fay <faya@msoe.edu>
2017-05-09 14:41:21 -0500

    It's a semicolon nightmaregit push origin hotfix/boaty-overdue!

--- src/lib/data-setup.js
+++ src/lib/data-setup.js
@@ -9,5 +9,5 @@ export function initData() {
     models.forEach(model => addAction('NEW_MODEL', model));
     items.forEach(item => addAction('NEW_ITEM', item));
     checkouts.forEach(checkout => addAction('NEW_CHECKOUT', checkout));
-    addAction('CHANGE_ITEM_DUEDATE', { studentId: '111111', dueDate: '1995-12-25', itemAddress: 'iGwEZVeaT' })
+    addAction('CHANGE_ITEM_DUEDATE', { studentId: '111111', dueDate: '1995-12-25', itemAddress: 'iGwEZVeaT' });
 }
